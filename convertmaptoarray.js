3884c71c0125c4a087830af6e974da98baba05cf
Rafael Trindade <rafael.silva@fineasytech.com>
2022-08-31 16:11:50 -0300

    JavaScript Convert Map To Array

---
+++ convertmaptoarray.js
@@ -0,0 +1,16 @@
+
+const language = new Map([
+
+    ["JavaScript", 1500],
+    ["ColdFusion", 1200],
+    ["PHP", 1000],
+    ["Python", 800]
+
+]);
+
+let myArray = Array.from(language);
+console.log(myArray);
+
+let myKeyArray = Array.from(language.keys());
+console.log(myKeyArray);
+
